094aaf36b19ff6ff3e820533b3754a9a6a598f6c
celengm <30536953+celengm@users.noreply.github.com>
2017-10-23 00:54:37 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,8 @@ const LineConnect = require('./connect');
 let LINE = require('./main1.js');
 
 const auth = {
-	authToken: ' token disini',
-	certificate: 'cert here',
+	authToken: ' EmZC87JkdSaYFSd3bEW5.ukgrBh1BCpARcVl0zqthXq.Ja5rw5ym9zIeyH7wr3kNRtBg+4noms9z7RQndCyvGU0=',
+	certificate: '0a7ee836407607ec4fd78c2c9762314f319096e293e25bf3d7bb940fedb345c8',
 }
 // let client =  new LineConnect(auth);
 let client =  new LineConnect();
