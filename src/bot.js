5e77ad2c754155442daff1f1ba5b79bac3c306e9
celengm <30536953+celengm@users.noreply.github.com>
2017-10-25 03:07:14 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -5,8 +5,8 @@ const auth = {
 	authToken: ' EmZC87JkdSaYFSd3bEW5.ukgrBh1BCpARcVl0zqthXq.Ja5rw5ym9zIeyH7wr3kNRtBg+4noms9z7RQndCyvGU0=',
 	certificate: '0a7ee836407607ec4fd78c2c9762314f319096e293e25bf3d7bb940fedb345c8',
 }
-// let client =  new LineConnect(auth);
-let client =  new LineConnect();
+ let client =  new LineConnect(auth);
+//let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
